e55f921298d86ed98dcd11799c1daed3ce45cbe8
Eevee <developerbrzoza@gmail.com>
2021-08-23 02:36:48 +0200

    description

--- EeveeStream.Web/resources/views/index.blade.php
+++ EeveeStream.Web/resources/views/index.blade.php
@@ -1,5 +1,31 @@
 @extends("app")
 
 @section("content")
-    <p>Hello!</p>
+    <span class="fixed_size_text">
+
+
+            <p>Welcome to <span style="color: hotpink">eevee!stream!</span> <br><br>
+
+                <span style="color: hotpink">eevee!stream!</span> is an attempt to recreate the old <a href="https://osustream.com" style="color: pink">osu!stream</a> experience on mobile. <br>
+            This has been done by recreating the entire backend of osu!stream and alot of testing. <br><br>
+
+            We are also using a slightly modified client for this which allows switching servers. <br>
+
+            <a href="https://github.com/peppy" style="color: lightblue">peppy</a> conviniently made osu!stream open source last year so this was pretty easy to do, <br><br>
+
+            Also on peppy's osu!stream Github repository, <br>
+
+            a user called "<a href="https://github.com/nyamimi" style="color: pink">nyamimi</a>" has attempted a Android port of osu!stream, <br>
+
+            And this is exactly what we are using for the mobile client, it has a few issues, but they are mostly minor. <br><br>
+
+            We also allow <span style="color: lightpink">Mapping!</span> Custom maps can be made and submitted here, <br>
+            If your map is up to quality standards, it may even be ranked! <br>
+            Ranked Beatmaps can be found on the <a href="beatmaps" style="color: lightblue">Beatmap Page</a>, <br>
+            On the <a href="info" style="color: lightblue">Information Page</a> you can also find various mapping related tutorials and guides to aid you in mapping.<br><br>
+
+            Why don't you get started! <br>
+            <b style="color: hotpink">Have fun playing!</b></p>
+
+    </span>
 @endsection
